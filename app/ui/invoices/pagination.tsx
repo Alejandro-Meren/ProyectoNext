622a572daa61b8001cec5e434188607588e59040
Alejandro Merenciano <alemeco2005@gmail.com>
2025-01-21 00:48:07 +0100

    CRUD casi acabado v2

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -9,11 +9,11 @@ import { usePathname, useSearchParams } from 'next/navigation';
 export default function Pagination({ totalPages }: { totalPages: number }) {
   const pathname = usePathname();
   const searchParams = useSearchParams();
-  const currentPage = Number(searchParams.get('page')) || 1;
+  const currentPage = searchParams ? Number(searchParams.get('page')) || 1 : 1;
   const allPages = generatePagination(currentPage, totalPages);
 
   const createPageURL = (pageNumber: number | string) => {
-    const params = new URLSearchParams(searchParams);
+    const params = new URLSearchParams(searchParams || '');
     params.set('page', pageNumber.toString());
     return `${pathname}?${params.toString()}`;
   };
