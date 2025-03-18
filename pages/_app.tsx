d4bbb3796bb5ffe47a51b18fb0d6a3de43ce1707
Alejandro Merenciano <alemeco2005@gmail.com>
2025-03-18 18:17:54 +0100

    Resolviendo errores v3

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
-import '../app/ui/global.css';
+import '../app/ui/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
