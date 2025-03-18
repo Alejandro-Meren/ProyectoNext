17f4da108f3afc753440ac3f9fc4171a9f0ec85d
Alejandro Merenciano <alemeco2005@gmail.com>
2025-03-18 19:35:21 +0100

    Responsive login form hair salon

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -6,8 +6,8 @@ export default function AcmeLogo() {
     <div
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
-      <ScissorsIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Hair Salon</p>
+      <ScissorsIcon className="h-10 w-10 sm:h-12 sm:w-12 rotate-[15deg]" />
+      <p className="text-[32px] sm:text-[44px] ml-2">Hair Salon</p>
     </div>
   );
 }
